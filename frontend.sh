aead43f8f97e2d6dbeccea8eb2cb579e8d819b7f
Ganesh <ganeshrao15061998@gmail.com>
2023-04-17 20:20:23 +0530

    frontend automate script

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,10 @@
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 #vim /etc/nginx/default.d/roboshop.conf
-cp C:/Users/dell 1/Desktop/devops-practice/Devops-Training/frontend.conf /etc/nginx/default.d/roboshop.conf
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
+
+systemctl enable nginx
